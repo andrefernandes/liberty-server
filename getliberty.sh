adc8f1c9c1b6c8ce7d33680da18c313e8e028d54
andre <goroh@hotmail.com>
2014-09-20 11:43:31 -0300

    get-liberty

---
+++ getliberty.sh
@@ -0,0 +1,3 @@
+wget https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/wasdev/downloads/wlp/8.5.5.3/wlp-developers-runtime-8.5.5.3.jar -q
+wget https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/wasdev/downloads/wlp/8.5.5.3/wlp-developers-extended-8.5.5.3.jar -q
+
